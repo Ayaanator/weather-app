5bb4bc76eaf7a5919e261c257737722be485230c
Ayaan Adrito <74221075+Ayaanator@users.noreply.github.com>
2025-09-05 13:11:11 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const stored_build = localStorage.getItem("BUILD_ID");
+
+if (stored_build != __BUILD_ID__) {
+  console.log("New build detected!");
+  const new_build_id = __BUILD_ID__;
+
+  localStorage.clear();
+  localStorage.setItem("BUILD_ID", new_build_id);
+}
+
+if (process.env.NODE_ENV === "development") {
+  console.log("Running in development mode!");
+} else if (process.env.NODE_ENV === "production") {
+  console.log("Running in production mode!");
+}
---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+    new webpack.DefinePlugin({
+      __BUILD_ID__: JSON.stringify(Date.now()), // or use [fullhash] if you want
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
